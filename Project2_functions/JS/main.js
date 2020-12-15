efc265ab6d0add4b559d99e2b85ca6f3d13e0c17
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2020-12-14 19:24:49 -0700

    push

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -1,5 +1,5 @@
 function myfunction() {
-    var sentence = "I am learning";
-    sentence += "a lot from this book!";
-    document.getElementById("Concatentate").innerHTML = sentence;
+    var sentence = "I am learning"; // This will show up in the front of the sentence //
+    sentence += " a lot from this book!"; // this line will show up after i am learning // 
+    document.getElementById("Concatentate").innerHTML = sentence; // this line is getting the id concatentate // 
 }
--- Projects1_expressions_alert/main.js
+++ Projects1_expressions_alert/main.js
@@ -2,13 +2,12 @@
 
  window.alert("Hellow, World!");
 
- var A = "";{
+ var A = "";
     document.write("Lisa told bart, \"Knock it off, Bart! or I\'ll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
- };
 
-var B = "concatenated" + "string";{
+var B = "concatenated" + "string";
      document.write(B);
-};
+
 
 var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";{
    document.write(Dad);
@@ -18,8 +17,13 @@ var blues = "I have the blues.";
 var blues = blues.fontcolor("blue");
 document.write(blues);
 
-function My_First_Function() { //Defining a function and naming it
+function My_First_Function() //Defining a function and naming it
    var str = "This text is green!"; //Defining a variable and giving it a string value
    var result = str.fontcolor("green"); //Using the fontcolor method on str variable
    document.getElementById("Green_Text").innerHTML = result; //Putting the value of result into HTML element with "Green_Text" id 
-}
+
+   var a = (b + c);
+   let sum = function(b,c) {
+      return b + c;
+      document.write(a);
+   };
